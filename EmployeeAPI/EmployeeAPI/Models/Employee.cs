ad8213ea2ba10871001b418e1959c5d8dc343abe
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-22 14:54:14 -0500

    Adding Employee model

--- EmployeeAPI/EmployeeAPI/Models/Employee.cs
+++ EmployeeAPI/EmployeeAPI/Models/Employee.cs
@@ -2,5 +2,15 @@
 {
     public class Employee
     {
+
+        private string Id;
+
+        private string Name {  get; set; }
+        private string Email { get; set; }
+        private int Phone { get; set; }
+        private int Address { get; set; }
+        private int salary { get; set; }
+        private string Department { get; set; }
+
     }
 }
