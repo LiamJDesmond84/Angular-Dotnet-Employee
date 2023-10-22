27aa06fc526058e19ae2e85b74e84c92db03d8b5
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-22 12:58:24 -0500

    Adding static data

--- EmployeeUI/src/app/components/employees/employee-list/employee-list.component.ts
+++ EmployeeUI/src/app/components/employees/employee-list/employee-list.component.ts
@@ -8,12 +8,48 @@ import { Employee } from 'src/app/models/employee.model';
 })
 export class EmployeeListComponent implements OnInit {
 
-  employees: Employee[] = [];
+  employees: Employee[] = [
+    {
+      "id": "1",
+      "name": "Bob",
+      "email":"bob@test.com",
+      "phone": 6195553434,
+      "salary": 78888,
+      "department": "security"
+    },
+    {
+      "id": "2",
+      "name": "Karl",
+      "email":"karl@test.com",
+      "phone": 61955534345,
+      "salary": 50000,
+      "department": "customer service"
+    },
+    {
+      "id": "3",
+      "name": "Sam",
+      "email":"same@test.com",
+      "phone": 6195553784,
+      "salary": 70000,
+      "department": "accounting"
+    }
+    ,{
+      "id": "3",
+      "name": "Steve",
+      "email":"steve@test.com",
+      "phone": 6195551134,
+      "salary": 80000,
+      "department": "engineering"
+    }
+  ];
 
   constructor() {}
 
   ngOnInit(): void {
 
+
+
+    this.employees.push()
 
   }
 
