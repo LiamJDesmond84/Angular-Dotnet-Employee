60d316557ca2bf365e5031f21d5eed425a30c15c
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-22 12:37:00 -0500

    Updating  employee-list component

--- EmployeeUI/src/app/components/employees/employee-list/employee-list.component.ts
+++ EmployeeUI/src/app/components/employees/employee-list/employee-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
+
+
+  ngOnInit(): void {
+
+  }
 
 }
