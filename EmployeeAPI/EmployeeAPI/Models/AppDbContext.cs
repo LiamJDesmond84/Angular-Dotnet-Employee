93aea6ce09d0c0c900cc0c9d5253b81a4209e713
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-22 14:57:48 -0500

    Adding DbContext

---
+++ EmployeeAPI/EmployeeAPI/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        DbSet<Employee> Employees { get; set;}
+
+    }
+}
