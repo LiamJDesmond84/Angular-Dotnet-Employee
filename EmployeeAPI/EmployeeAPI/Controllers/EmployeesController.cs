39dc378234e98d9c44f8fd49c5bbf27e73b97807
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-23 19:33:30 -0500

    Added EmployeesController

---
+++ EmployeeAPI/EmployeeAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAPI.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
