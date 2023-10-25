4f9efcf45fa793374ab210f5e63ef026e17a52cb
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-24 21:35:43 -0500

    Will have to do this tomorrow

--- EmployeeUI/src/app/components/employees/employee-list/employee-list.component.ts
+++ EmployeeUI/src/app/components/employees/employee-list/employee-list.component.ts
@@ -18,38 +18,38 @@ export class EmployeeListComponent implements OnInit {
 
 
 
-    this.employees.push(    {
-      "id": "1",
-      "name": "Bob",
-      "email":"bob@test.com",
-      "phone": 6195553434,
-      "salary": 78888,
-      "department": "security"
-    },
-    {
-      "id": "2",
-      "name": "Karl",
-      "email":"karl@test.com",
-      "phone": 61955534345,
-      "salary": 50000,
-      "department": "customer service"
-    },
-    {
-      "id": "3",
-      "name": "Sam",
-      "email":"same@test.com",
-      "phone": 6195553784,
-      "salary": 70000,
-      "department": "accounting"
-    }
-    ,{
-      "id": "3",
-      "name": "Steve",
-      "email":"steve@test.com",
-      "phone": 6195551134,
-      "salary": 80000,
-      "department": "engineering"
-    })
+    // this.employees.push(    {
+    //   "id": "1",
+    //   "name": "Bob",
+    //   "email":"bob@test.com",
+    //   "phone": 6195553434,
+    //   "salary": 78888,
+    //   "department": "security"
+    // },
+    // {
+    //   "id": "2",
+    //   "name": "Karl",
+    //   "email":"karl@test.com",
+    //   "phone": 61955534345,
+    //   "salary": 50000,
+    //   "department": "customer service"
+    // },
+    // {
+    //   "id": "3",
+    //   "name": "Sam",
+    //   "email":"same@test.com",
+    //   "phone": 6195553784,
+    //   "salary": 70000,
+    //   "department": "accounting"
+    // }
+    // ,{
+    //   "id": "3",
+    //   "name": "Steve",
+    //   "email":"steve@test.com",
+    //   "phone": 6195551134,
+    //   "salary": 80000,
+    //   "department": "engineering"
+    // })
 
   }
 
