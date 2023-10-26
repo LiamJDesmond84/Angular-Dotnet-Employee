f99ad1d7325e80e6c3997c3fa62b6de316186d8d
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-26 16:10:54 -0500

    Updating employee service

--- EmployeeUI/src/app/services/employees.service.ts
+++ EmployeeUI/src/app/services/employees.service.ts
@@ -1,10 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeesService {
+
+  baseApiUrl: string = environment.baseApiUrl;
 
   constructor(private http: HttpClient) { }
 
