346f2d38b2fd2a4eca40ecc4b01da50cdea4f62b
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-26 16:21:25 -0500

    Updating app.ts

--- EmployeeUI/src/app/app.module.ts
+++ EmployeeUI/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './components/employees/employee-list/employee-list.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { EmployeeListComponent } from './components/employees/employee-list/empl
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
