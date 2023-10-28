322736df2da5559612613acb452f334860ed0898
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-28 16:13:31 -0500

    Adding Http post method

--- EmployeeUI/src/app/components/employees/add-employee/add-employee.component.ts
+++ EmployeeUI/src/app/components/employees/add-employee/add-employee.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent {
+
+  constructor(http: HttpClient){}
+
+
 
 }
