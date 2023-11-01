19326370c340da227384255b09e8a6c142a61d52
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-01 18:45:40 -0500

    Updating editEmployee component method

--- EmployeeUI/src/app/components/employees/edit-employee/edit-employee.component.ts
+++ EmployeeUI/src/app/components/employees/edit-employee/edit-employee.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
   selector: 'app-edit-employee',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class EditEmployeeComponent {
 
+  editEmployeeRequest: Employee = {
+    id: "",
+    name: "",
+    email: "",
+    phone: 0,
+    salary: 0,
+    department: ""
+  }
+
+  constructor(private employeeService: EmployeesService) {}
+
+  public editEmployee() {
+
+    this.employeeService.editEmployee(this.editEmployeeRequest)
+    .subscribe().
+  }
 }
