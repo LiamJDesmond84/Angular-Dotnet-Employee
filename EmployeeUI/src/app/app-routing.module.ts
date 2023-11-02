5a43b9b459b45dab1bf44bf03171a78d06de1a26
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-02 18:18:10 -0500

    Updating app-routing

--- EmployeeUI/src/app/app-routing.module.ts
+++ EmployeeUI/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './components/employees/employee-list/employee-list.component';
 import { AddEmployeeComponent } from './components/employees/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/employees/edit-employee/edit-employee.component';
 
 const routes: Routes = [
   // {
@@ -15,7 +16,12 @@ const routes: Routes = [
   {
     path: "employees/add",
     component: AddEmployeeComponent
+  },
+  {
+    path: "employees/edit",
+    component: EditEmployeeComponent
   }
+
 ];
 
 @NgModule({
