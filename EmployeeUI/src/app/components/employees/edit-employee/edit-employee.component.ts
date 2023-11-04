a9079f075758fe5412b295763ca7f19e8f7070f2
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-04 14:47:09 -0500

    Updating edit component

--- EmployeeUI/src/app/components/employees/edit-employee/edit-employee.component.ts
+++ EmployeeUI/src/app/components/employees/edit-employee/edit-employee.component.ts
@@ -32,7 +32,7 @@ export class EditEmployeeComponent implements OnInit {
             this.employeeService.getEmployee(id)
             .subscribe({
               next: (response) => {
-                retur
+                this.employeeDetails = response;
               }
             });
           }
