5367ba5844adc30407061ec81b5a628ddccada9a
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-04 15:51:32 -0500

    Updating Program.cs

--- EmployeeAPI/EmployeeAPI/Program.cs
+++ EmployeeAPI/EmployeeAPI/Program.cs
@@ -26,7 +26,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors(policy => policy.AllowAnyHeader().AllowAnyHeader().AllowAnyOrigin());
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
 app.UseAuthorization();
 
