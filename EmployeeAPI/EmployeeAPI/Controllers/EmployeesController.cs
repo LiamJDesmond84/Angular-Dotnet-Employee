fa7cafc6cd616ff5e5f98352a6f22a0f1891da3a
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-04 15:00:40 -0500

    Adding update method to API

--- EmployeeAPI/EmployeeAPI/Controllers/EmployeesController.cs
+++ EmployeeAPI/EmployeeAPI/Controllers/EmployeesController.cs
@@ -74,11 +74,13 @@ namespace EmployeeAPI.Controllers
             }
             else
             {
-                employee.Name = employeeUpdateRequest.Name;
-                employee.Email = employeeUpdateRequest.Email;
-                employee.Salary = employeeUpdateRequest.Salary;
-                employee.Phone = employeeUpdateRequest.Phone;
-                employee.Department = employeeUpdateRequest.Department;
+                //employee.Name = employeeUpdateRequest.Name;
+                //employee.Email = employeeUpdateRequest.Email;
+                //employee.Salary = employeeUpdateRequest.Salary;
+                //employee.Phone = employeeUpdateRequest.Phone;
+                //employee.Department = employeeUpdateRequest.Department;
+
+                employee = employeeUpdateRequest;
 
                 await context.SaveChangesAsync();
                 return Ok(employee);
